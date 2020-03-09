37d2c9320d91676e9401c2c241bbc555791d1f25
lipei5 <lipei5@lenovo.com>
2020-03-09 16:32:09 +0800

    add server ip

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 var config = {
     DBConfig: {
         connectionLimit: 10,
-        host:'127.0.0.1',
+        // host:'127.0.0.1',
+        host:'http://47.116.6.139/',
         port:'3306',
         user:'root',
         password:'root',
