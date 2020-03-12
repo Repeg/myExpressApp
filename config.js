d312a51b7df8bb16c302cd68abcb49a03386fc6b
lipei5 <lipei5@lenovo.com>
2020-03-12 22:16:23 +0800

    1

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ var config = {
     DBConfig: {
         connectionLimit: 10,
         host:'127.0.0.1',
-        password:'root',
+        // password:'root',
         // host:'47.116.6.139',
-        // password:'Li19931006pei',
+        password:'Li19931006pei',
         port:'3306',
         user:'root',
         database:'test'
