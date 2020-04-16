2bf934368ba1a5cb63bf63819a46404df7de68fc
hongzhulp <1097962590@qq.com>
2020-04-16 22:37:03 +0800

    sql fix

--- DB/userApi.js
+++ DB/userApi.js
@@ -1,7 +1,7 @@
 var userApi = {
     getALLUser: 'select * from user',
     saveUserInfo: 'INSERT INTO user (id, openid, name, nickName, avatarUrl, gender, city, province, country) VALUES (0,?,?,?,?,?,?,?,?)',
-    updateUserInfo: 'UPDATE user SET name=? and nickName=?,avatarUrl=?,gender=?,city=?,province=?,country=? WHERE openid=?',
+    updateUserInfo: 'UPDATE user SET name=?,nickName=?,avatarUrl=?,gender=?,city=?,province=?,country=? WHERE openid=?',
     getUserWithOpenid: 'select * from user WHERE openid = ?',
     updateTokenByOpenid: "UPDATE user SET token=? WHERE openid=?",
     insertUserTokenAndOpenid: "INSERT INTO user (id, openid, token) VALUES (0,?,?)"
