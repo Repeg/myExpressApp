48bf15194349fa5285f796844f7e14814362ee83
hongzhulp <1097962590@qq.com>
2020-04-16 22:09:12 +0800

    sql change

--- routes/users.js
+++ routes/users.js
@@ -20,21 +20,28 @@ router.post('/saveUserInfo', function(req, res, next) {
   if(req.body.nickName && req.body.avatarUrl && req.body.openid){
     var data = req.body;
     query(userApi.getUserWithOpenid,[req.body.openid],(err1,res1)=>{
-      console.log("------getUserWithOpenid-----_data.openid----" + new Date() + "------"+ new Date().getTime() +"---------------");
+      console.log("---------saveUserInfo----getUserWithOpenid---openid-" + req.body.openid + "----" + new Date() + "------"+ new Date().getTime() +"---------------");
       console.log(err1,res1);
-      var api = userApi.saveUserInfo;
-      var queryData = [data.openid, data.nickName, data.nickName, data.avatarUrl, data.gender, data.city, data.province, data.country];
-      if(res1.length > 0){
-        api = userApi.updateUserInfo;
-        queryData = [data.nickName, data.nickName, data.avatarUrl, data.gender, data.city, data.province, data.country, data.openid];
-      }
-      query(api,queryData,(error,queryRes)=>{
-        console.log(error,queryRes);
+      if(err1.length>0){
         res.json({
-          "success": true,
-          "msg": "insert success"
+          "success": false,
+          "msg": err1
         });
-      })
+      }else{
+        var api = userApi.saveUserInfo;
+        var queryData = [data.openid, data.nickName, data.nickName, data.avatarUrl, data.gender, data.city, data.province, data.country];
+        if(res1.length > 0){
+          api = userApi.updateUserInfo;
+          queryData = [data.nickName, data.nickName, data.avatarUrl, data.gender, data.city, data.province, data.country, data.openid];
+        }
+        query(api,queryData,(error,queryRes)=>{
+          console.log(error,queryRes);
+          res.json({
+            "success": true,
+            "msg": "insert success"
+          });
+        })
+      }
     })
   }else{
     res.json({
