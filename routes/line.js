08bb8b4bdfef30b67c0b0b61abe8e6d318b46e50
hongzhulp <1097962590@qq.com>
2020-04-18 19:10:19 +0800

    add queryCount func

--- routes/line.js
+++ routes/line.js
@@ -6,18 +6,29 @@ var lineApi = require('../DB/lineApi');
 var lineApi = lineApi.lineApi;
 var query = query.query;
 
+var timeRangeList = [60000,300000,600000,1800000,3600000,21600000,43200000,86400000,604800000,2592000000,31536000000,1586867717324];
+
 router.post('/addOne', function(req, res, next) {
     console.log("-------addOne--------" + new Date() + "------"+ new Date().getTime() +"---------------");
     var time = new Date().getTime();
     var openid = req.body.openid;
+    var timeRange = req.body.timeRange;
     query(lineApi.addOne,[time, openid],(err1,res1)=>{
         if(res1){
-            res.json({
-                "id": res1.insertId,
-                "time": time,
-                "success": true,
-                "msg": "insert addOne success"
-            });
+            queryCount(timeRange,(successRes)=>{
+                res.json({
+                    "id": res1.insertId,
+                    "count": successRes.length,
+                    "time": time,
+                    "success": true,
+                    "msg": "insert addOne success"
+                });
+            },(failRes)=>{
+                res.json({
+                    "success": false,
+                    "msg": failRes
+                });
+            })
         }else{
             res.json({
                 "success": false,
@@ -27,32 +38,40 @@ router.post('/addOne', function(req, res, next) {
     })
 })
 
-router.get('/getTimeRangeCount', function(req, res, next) {
-    console.log("-------getTimeRangeCount--------" + new Date() + "------"+ new Date().getTime() +"---------------");
-    var timeRangeList = [60000,300000,600000,1800000,3600000,21600000,43200000,86400000,604800000,2592000000,31536000000,1586867717324];
-    var tiemRange = req.query.timeRange;
-    var timeNow = new Date().getTime();
+function queryCount(timeRange,successCallBack,failCallBack){
     var timeStart;
-    if(tiemRange == 11){
+    var timeNow = new Date().getTime();
+    timeRange = parseInt(timeRange);
+    if(timeRange == 11){
         timeStart = timeRangeList[11];
     }else{
-        timeStart = timeNow - timeRangeList[tiemRange];
+        timeStart = timeNow - timeRangeList[timeRange];
     }
     query(lineApi.getTimeRangeCount,[timeStart, timeNow],(err1,res1)=>{
         if(res1){
-            res.json({
-                "count": res1.length,
-                "list": res1,
-                "success": true,
-                "msg": "getTimeRangeCount success"
-            });
+            successCallBack(res1);
         }else{
-            res.json({
-                "success": false,
-                "msg": err1
-            });
+            failCallBack(err1);
         }
     })
+}
+
+router.get('/getTimeRangeCount', function(req, res, next) {
+    console.log("-------getTimeRangeCount--------" + new Date() + "------"+ new Date().getTime() +"---------------");
+    var timeRange = req.query.timeRange;
+    queryCount(timeRange,(successRes)=>{
+        res.json({
+            "count": successRes.length,
+            "list": successRes,
+            "success": true,
+            "msg": "getTimeRangeCount success"
+        });
+    },(failRes)=>{
+        res.json({
+            "success": false,
+            "msg": failRes
+        });
+    });
 })
 
 module.exports = router;
