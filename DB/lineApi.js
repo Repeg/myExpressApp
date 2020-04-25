4128851918efe012e55a6bf331df67df02c582a1
hongzhulp <1097962590@qq.com>
2020-04-25 11:21:08 +0800

    line api done

--- DB/lineApi.js
+++ DB/lineApi.js
@@ -2,7 +2,7 @@ var lineApi = {
     addOne: 'INSERT INTO line (id, timeStamp, useropenid, linesId) VALUES (0,?,?,?)',
     add_user_line: 'INSERT INTO user_line (id, userOpenId, linesId) VALUES (0,?,?)',
     user_line_check: 'SELECT linesId FROM user_line WHERE userOpenId = ? and linesId = ?',
-    getTimeRangeCount: 'SELECT COUNT(*) from line WHERE timeStamp > ? and timeStamp < ? and linesId = ?',
+    getTimeRangeCount: 'SELECT * from line WHERE timeStamp > ? and timeStamp < ? and linesId = ?',
     getUserLineList:    `select line.linesId,count(*) AS count,allLine.lineName from line 
                         left JOIN allLine
                         on allLine.id = line.linesId
