3ef6f5b2ca0381be50d97646399755b7b330a729
lipei5 <lipei@lenovo.com>
2023-05-15 10:00:40 +0800

    user api update

--- DB/userApi.js
+++ DB/userApi.js
@@ -1,10 +1,8 @@
 var userApi = {
-    getALLUser: 'select * from users',
-    saveUserInfo: 'INSERT INTO user (id, openid, name, nickName, avatarUrl, gender, city, province, country) VALUES (0,?,?,?,?,?,?,?,?)',
-    updateUserInfo: 'UPDATE user SET name=?,nickName=?,avatarUrl=?,gender=?,city=?,province=?,country=? WHERE openid=?',
-    getUserWithOpenid: 'select * from user WHERE openid = ?',
-    updateTokenByOpenid: "UPDATE user SET token=? WHERE openid=?",
-    insertUserTokenAndOpenid: "INSERT INTO user (id, openid, token) VALUES (0,?,?)"
+    getALLUser: 'select * from user',
+    saveUserInfo: 'INSERT INTO user (id, userName, avatar) VALUES (0,?,?)',
+    updateUserInfo: 'UPDATE user SET userName=?,avatar=? WHERE id=?',
+    getUserWithId: 'select * from user WHERE id = ?',
 }
 
 exports.userApi = userApi;
--- routes/users.js
+++ routes/users.js
@@ -15,9 +15,53 @@ router.get('/getAllUserInfo', function(req, res, next) {
   })
 });
 
+// router.post('/saveUserInfo', function(req, res, next) {
+//   console.log("---------------" + new Date() + "------"+ new Date().getTime() +"---------------");
+//   if(req.body.nickName && req.body.avatarUrl && req.body.openid){
+//     var data = req.body;
+//     query(userApi.getUserWithOpenid,[req.body.openid],(err1,res1)=>{
+//       console.log("---------saveUserInfo----getUserWithOpenid---openid-" + req.body.openid + "----" + new Date() + "------"+ new Date().getTime() +"---------------");
+//       console.log(err1,res1);
+//       if(err1){
+//         res.json({
+//           "success": false,
+//           "msg": err1
+//         });
+//       }else{
+//         var msg = 'new';
+//         var api = userApi.saveUserInfo;
+//         var queryData = [data.openid, data.nickName, data.nickName, data.avatarUrl, data.gender, data.city, data.province, data.country];
+//         if(res1.length > 0){
+//           api = userApi.updateUserInfo;
+//           queryData = [data.nickName, data.nickName, data.avatarUrl, data.gender, data.city, data.province, data.country, data.openid];
+//           msg = "";
+//         }
+//         query(api,queryData,(error,queryRes)=>{
+//           console.log(error,queryRes);
+//           if(error){
+//             res.json({
+//               "success": false,
+//               "msg": error
+//             });
+//           }else{
+//             res.json({
+//               "success": true,
+//               "msg": "saveUserInfo success " + msg
+//             });
+//           }
+//         })
+//       }
+//     })
+//   }else{
+//     res.json({
+//       "success": false,
+//       "msg": "params missing"
+//     });
+//   }
+// });
 router.post('/saveUserInfo', function(req, res, next) {
   console.log("---------------" + new Date() + "------"+ new Date().getTime() +"---------------");
-  if(req.body.nickName && req.body.avatarUrl && req.body.openid){
+  if(req.body.userName && req.body.avatar){
     var data = req.body;
     query(userApi.getUserWithOpenid,[req.body.openid],(err1,res1)=>{
       console.log("---------saveUserInfo----getUserWithOpenid---openid-" + req.body.openid + "----" + new Date() + "------"+ new Date().getTime() +"---------------");
